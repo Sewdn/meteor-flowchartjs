735ae0d085e378d77832ff1f21a18fcdc9f5950e
Pieter Soudan <pieter.soudan@redandivory.be>
2014-08-12 14:19:11 +0200

    fix typo

--- package.js
+++ package.js
@@ -4,7 +4,7 @@ Package.describe({
 
 Package.on_use(function (api) {
   api.use(['jquery', 'raphael'], 'client');
-  api.add_files('lib/release/fowchart-1.3.2.js', 'client');
+  api.add_files('lib/release/flowchart-1.3.2.js', 'client');
   api.add_files('export.js', 'client');
   api.export('Flowchart');
 });
